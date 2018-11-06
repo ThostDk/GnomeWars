95af8a76246917d95b70f55039fc2612bc9532ea
ThostDk <44801529+ThostDk@users.noreply.github.com>
2018-11-06 10:35:33 +0100

    Add files via upload
    vores spilprojekt

---
+++ GnomeWars/GnomeWars/Sprite.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GnomeWars
+{
+    class Sprite
+    {
+        public Vector2 Position = new Vector2(0, 0);
+        protected Texture2D spriteTexture;
+
+        public void LoadContent(ContentManager theContentManager, string myAssetName)
+        {
+            spriteTexture = theContentManager.Load<Texture2D>(myAssetName);
+        }
+    }
+}
